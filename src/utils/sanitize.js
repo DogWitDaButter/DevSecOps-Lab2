470bc3a833ce1f977e4777d5efa04f72337a7a9b
Seif <saifeddine.jeder@insat.ucar.tn>
2025-10-26 23:40:21 +0100

    failure

--- src/utils/sanitize.js
+++
@@ -1,8 +0,0 @@
-export function sanitizeInput(input) {
-  // Remove script tags *and their content*
-  const noScript = input.replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
-  // Remove any remaining HTML tags
-  const noTags = noScript.replace(/<[^>]*>/g, '');
-  // Trim extra spaces
-  return noTags.trim();
-}
