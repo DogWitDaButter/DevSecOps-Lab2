3d33fe26d1e0ae3f45f9470f7d4f4458b2e9801e
Seif <saifeddine.jeder@insat.ucar.tn>
2025-10-26 23:37:55 +0100

    failure 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
   return (
-    <div>
-      <h1>Welcome to My Secure React App</h1>
-      <button onClick={() => alert('Clicked!')}>Click me</button>
+    <div className="App">
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <h1>Hello {name}</h1> {/* XSS vulnerability */}
     </div>
   );
 }
